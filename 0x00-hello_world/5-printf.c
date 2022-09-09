f3d2590abf7cce5106a75c99f53a641aa8b0526c
miracle352 <tobilobaiyanuoluwa352@gmail>
2022-09-09 06:07:36 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
+
+
 /**
+ *
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -9,8 +12,11 @@
 int main(void)
 
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n")
 
-	return (0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+
+
+		return (0);
 
 }
