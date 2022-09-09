1b7762d823e339aef28b5305d0b41a801068826a
miracle352 <tobilobaiyanuoluwa352@gmail>
2022-09-09 06:01:57 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -8,8 +9,8 @@
 int main(void)
 
 {
-			printf("with proper grammar, but the outcome is a piece of art,\n")
+		printf("with proper grammar, but the outcome is a piece of art,\n")
 
-	    return (0);
+return (0);
 
 }
