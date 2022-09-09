326f7219484b5f4c157118d5c49b776b4095859a
miracle352 <tobilobaiyanuoluwa352@gmail>
2022-09-09 02:04:16 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 /**
  *main - Entry point
+ *
+ *Return: Always 0 (Sucess)
  */
 
 int main(void)
 	{
 		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+		return (0);
 
 	}
