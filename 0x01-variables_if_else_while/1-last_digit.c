ccac31ed35a5503f77e86d675e1e47958f8cbdef
miracle352 <tobilobaiyanuoluwa352@gmail>
2022-09-09 16:32:41 -0700

    c Programming

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,14 +7,10 @@
 
 
 /**
- *
- *  * main - assign a random number to the variable n each time it is executed
- *
- *   * The string Last digit of n
- *
- *    * Return: 0
- *
- *     */
+ * main - assign a random number to the variable n each time it is executed
+ * The string Last digit of n
+ * Return: 0
+ */
 
 
 
