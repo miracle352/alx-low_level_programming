2d2fe41ac217d758e6b93d046810dbc41be01c08
miracle352 <tobilobaiyanuoluwa352@gmail>
2022-09-09 00:15:31 -0700

    c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,24 @@
-#include <stdio.h>
+ #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- * /
-int main(void)
-{
-	char a;
-	int b;
-	long int c;
-	lonng long int d;
-	float e;
-
-
-	printf("size of a char: %zu byte(s)\n", sizeof(a);
+ /**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  * /
+ int main(void)
+ { 
+ 	char a;
+ 	int b;
+ 	long int c;
+ 	lonng long int d;
+ 	float e;
+ 
+ 
+ 	printf("size of a char: %zu byte(s)\n", sizeof(a);
 	printf("size of int : %zu byte(s)\n", sizeof(B));
-	printf("size of s long int: %zu byte(s)\n", sizeof(c));
-	printf("Size of a long long int: %ku byte(s)\n", sizeof(d));
-	printf("Size of a float: %zu bytes (s)\n", sizeof(d));
-
-	return (0);
-}
+ 	printf("size of s long int: %zu byte(s)\n", sizeof(c));
+ 	printf("Size of a long long int: %ku byte(s)\n", sizeof(d));
+ 	printf("Size of a float: %zu bytes (s)\n", sizeof(d));
+ 
+ 	return (0);
+} 
