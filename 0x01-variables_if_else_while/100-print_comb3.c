0bf029dc1d989f3fefb4dfae32ea7f19b1dbfe93
miracle352 <tobilobaiyanuoluwa352@gmail>
2022-09-10 12:53:53 -0700

    c program

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
  * main - program that prints all possible different combinations of two digits
  * Numbers must be separated by ,followed by a space
@@ -15,7 +13,6 @@
 int main(void)
 
 {
-
 	int n1 = 48;
 
 	int n2;
@@ -25,13 +22,11 @@ int main(void)
 	while (n1 <= 57)
 
 	{
-
 		n2 = n1 + 1;
 
 		while (n2 <= 57)
 
 		{
-
 			putchar(n1);
 
 			putchar(n2);
@@ -39,23 +34,17 @@ int main(void)
 			if (n1 != 56 || n2 != 57)
 
 			{
-
 				putchar(com);
 
 				putchar(32);
 
 			}
-
 			n2 += 1;
 
 		}
-
 		n1 += 1;
 
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
