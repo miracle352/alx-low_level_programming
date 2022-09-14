4b8b5492f1311b363104e47fd2df720c63bd2456
miracle352 <tobilobaiyanuoluwa352@gmail>
2022-09-14 16:00:01 -0700

    printing alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function to print abc 10 times
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char a, b;
+
+	for (b = 0; b < 10; b++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
