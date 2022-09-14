2057dc68e607e2646ae87106a6d4673d4a09266e
miracle352 <tobilobaiyanuoluwa352@gmail>
2022-09-14 15:52:45 -0700

    c functions and loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,15 +5,13 @@
  * You can only use _putchar twice in your code
  * Return: nothing.
  */
-
-void print_alphabet (void)
+void print_alphabet(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
-	
 	}
 	_putchar('\n');
 }
