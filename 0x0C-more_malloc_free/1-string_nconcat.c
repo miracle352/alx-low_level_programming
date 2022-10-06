1b082aeeff158e13053aa3d89e05a3dd59843456
miracle352 <tobilobaiyanuoluwa352@gmail>
2022-10-06 16:33:20 -0700

    malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,16 +20,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s2 == NULL)
 		s2 = "";
-
 	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
 		;
-
 	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
 		;
-
 	if (n > ls2)
 		n = ls2;
-
 	lsout = ls1 + n;
 
 	sout = malloc(lsout + 1);
@@ -42,8 +38,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			sout[i] = s1[i];
 		else
 			sout[i] = s2[i - ls1];
-
 	sout[i] = '\0';
-
 	return (sout);
 }
